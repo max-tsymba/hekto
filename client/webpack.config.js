17f8b0568a38f2b0dec80caf0393d131c5077e63
max tsymbaliuk <max.tsymbaliuk@wwwest.solutions>
2021-11-01 00:41:22 +0200

    webpack config js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,4 +9,23 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].[hash].js"
     },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: "./src/index.html"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)/,
+                use: ['file-loader']
+            }
+        ]
+    }
 }
