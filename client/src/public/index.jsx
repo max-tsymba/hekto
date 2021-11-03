5660ba92c2a0021caa5e04f2330ff003ebac0d49
max tsymbaliuk <max.tsymbaliuk@wwwest.solutions>
2021-11-03 18:45:22 +0200

    update index

--- client/src/public/index.jsx
+++ client/src/public/index.jsx
@@ -3,12 +3,14 @@ import React, {createContext} from 'react';
 import { render } from 'react-dom';
 import App from '../App.jsx';
 import UserStore from '../store/userStore';
+import FurnitureStore from '../store/furnitureStore';
 
 export const Context = createContext(null);
 
 render(
     <Context.Provider value={{
-        user: new UserStore()
+        user: new UserStore(),
+        furniture: new FurnitureStore()
     }}>
         <App/>
     </Context.Provider>,
